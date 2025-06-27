c75f09c9c67d2c1bad36483bd5141de303d20bcd
Léo Genot <45610291+leogenot@users.noreply.github.com>
2025-06-27 14:07:22 +0200

    update cli config

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,13 +1,16 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'y1aacj2s',
-    dataset: 'production'
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  studioHost: process.env.SANITY_STUDIO_HOSTNAME,
+  vite: {
+    esbuild: {
+      supported: {
+        'top-level-await': true
+      },
+    },
+  },
 })
